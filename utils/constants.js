e8cb742490301fa2a0740f7c43d8a439b539aee0
Hima Deepika Mannam <himad.mannam@gmail.com>
2024-05-05 00:45:52 -0500

    changing readme file

---
+++ utils/constants.js
@@ -0,0 +1,16 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x6b1f68e731473122806deb84233abfb506774fbe";
+export const contractABI = abi.abi;
+
+/**
+ 
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const contractABI = abi.abi;
+
+ 0x1259077EE93B918623Db206dF0c4b68dDC05a721 
+ Transactions address: 0x62feD83F5fE7a4aC41394359973A3625AeEC0b33
+
+ */
